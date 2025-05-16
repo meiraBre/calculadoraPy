8b00cd6afb30197ebd235c466f13a36dfc62d28b
Brenda <bremeirah@gmail.com>
2025-05-16 16:26:09 -0300

    Calculadora criada com as 4 operações e mais 2 que escolhi

---
+++ Calculadora/operacoes.py
@@ -0,0 +1,18 @@
+def somar_numeros (num1, num2):
+    return num1 + num2
+
+def subtrair_numeros (num1, num2):
+    return num1 - num2
+
+def dividir_numeros (num1, num2):
+    return num1 / num2
+
+def multiplicar_numeros (num1, num2):
+    return num1 * num2
+
+def elevar_numeros (num1, num2):
+    return num1 ** num2
+
+def extrair_raiz_quadrada (num1):
+    return num1 ** 0.5
+#Utilização de IA para fazer a função de raiz quadrada
