4c7140bd5add16fefab2388cb9711d8067e227fc
Brenda <bremeirah@gmail.com>
2025-05-16 16:45:22 -0300

    Deixando os métodos mais robustos

--- Calculadora/operacoes.py
+++ Calculadora/operacoes.py
@@ -1,18 +1,23 @@
-def somar_numeros (num1, num2):
-    return num1 + num2
+class Calculadora:
+    def somar_numeros (num1, num2):
+        return num1 + num2
 
-def subtrair_numeros (num1, num2):
-    return num1 - num2
+    def subtrair_numeros (num1, num2):
+        return num1 - num2
 
-def dividir_numeros (num1, num2):
-    return num1 / num2
+    def dividir_numeros (num1, num2):
+        if num2 == 0:
+            raise ValueError("Não digite zero para realizar a operação de divisão") 
+        return num1 / num2
 
-def multiplicar_numeros (num1, num2):
-    return num1 * num2
+    def multiplicar_numeros (num1, num2):
+        return num1 * num2
+    
+    def elevar_numeros (num1, num2):
+        return num1 ** num2
 
-def elevar_numeros (num1, num2):
-    return num1 ** num2
-
-def extrair_raiz_quadrada (num1):
-    return num1 ** 0.5
+    def extrair_raiz_quadrada (num1):
+        if num1 < 0 :
+            raise ValueError("Não é possível realizar contas de raiz quadrada com número negativo")
+        return num1 ** 0.5
 #Utilização de IA para fazer a função de raiz quadrada
